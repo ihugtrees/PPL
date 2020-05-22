8594722baaa5063629d1780772f16a8847f94939
Igal <igoralikin@gmail.com>
2020-05-23 00:29:31 +0300

    yalla
    Co-authored-by: HaimTempo <volin@post.bgu.ac.il>

--- hw3/part2/mermaid.ts
+++ hw3/part2/mermaid.ts
@@ -1,123 +1,199 @@
-import { Parsed, isProgram, isAtomicExp, isCompoundExp, AtomicExp, isNumExp, isBoolExp, isStrExp, isPrimOp, isVarRef, Exp, isAppExp, DefineExp, isDefineExp, isIfExp, isProcExp, isLetExp, isLetrecExp, isLitExp, isSetExp, Program, LitExp, CExp, isVarDecl, ProcExp, isExp, VarDecl } from "./L4-ast";
+import { Parsed, isProgram, isAtomicExp, isCompoundExp, AtomicExp, isNumExp, isBoolExp, isStrExp, isPrimOp, isVarRef, Exp, isAppExp, DefineExp, isDefineExp, isIfExp, isProcExp, isLetExp, isLetrecExp, isLitExp, isSetExp, Program, LitExp, CExp, isVarDecl, ProcExp, isExp, VarDecl, makeVarRef, isBinding } from "./L4-ast";
 import { Result, bind, makeOk, mapResult, makeFailure } from "../shared/result";
 import { Graph, GraphContent, makeGraph, makeDir, CompoundGraph, makeCompoundGraph, AtomicGraph, makeAtomicGraph, makeNodeDecl, Edge, makeNodeRef, makeEdge, Node, NodeDecl, NodeRef } from "./mermaid-ast";
 import { SExpValue, isCompoundSExp } from "./L4-value";
 import { map, concat } from "ramda";
 import { isEmpty, rest } from "../shared/list";
+import { makeVarGen } from "../L3/substitute";
 
 export const mapL4toMermaid = (exp: Parsed): Result<Graph> =>
 	bind(l4GraphContent(exp), (graphCont: GraphContent) => makeOk(makeGraph(makeDir("TD"), graphCont)))
 
 const l4GraphContent = (exp: Parsed): Result<GraphContent> =>
-	isAtomicExp(exp) ? makeAtomicTree(exp) :
+	isAtomicExp(exp) ? makeOk(makeAtomicGraph(makeAtomicDecl(exp))) :
 		isProgram(exp) ? makeOk(makeCompoundGraph(createProgram(exp, counter("Exps")))) :
-			isExp(exp) ? makeOk(makeCompoundGraph(createExp(exp, ""))) :
-				// isDefineExp(exp) ? makeOk(makeCompoundGraph(createExp(exp,""))) :
-				//     isAppExp(exp) ? makeOk(makeCompoundGraph(createExp(exp))) :
-				//         isIfExp(exp) ? makeOk(makeCompoundGraph(createExp(exp))) :
-				//             isProcExp(exp) ? makeOk(makeCompoundGraph(createExp(exp))) :
-				//                 isLetExp(exp) ? makeOk(makeCompoundGraph(createExp(exp))) :
-				//                     isLetrecExp(exp) ? makeOk(makeCompoundGraph(createExp(exp))) :
-				//                         isLitExp(exp) ? makeOk(makeCompoundGraph(createExp(exp))) :
-				//                             isSetExp(exp) ? makeOk(makeCompoundGraph(createExp(exp))) :
+			isExp(exp) ? makeOk(makeCompoundGraph(createExpEdges(exp, ""))) :
 				makeFailure("error")
 
-const makeAtomicTree = (exp: AtomicExp): Result<AtomicGraph> =>
-	isNumExp(exp) ? makeOk(makeAtomicGraph(makeNodeDecl(counter("number"), `number("${exp.val}")`))) :
-		isBoolExp(exp) ? makeOk(makeAtomicGraph(makeNodeDecl(counter("boolean"), `boolean("${exp.val}")`))) :
-			isStrExp(exp) ? makeOk(makeAtomicGraph(makeNodeDecl(counter("string"), `string("${exp.val}")`))) :
-				isPrimOp(exp) ? makeOk(makeAtomicGraph(makeNodeDecl(counter("PrimOp"), `PrimOp("${exp.op}")`))) :
-					isVarRef(exp) ? makeOk(makeAtomicGraph(makeNodeDecl(counter("VarRef"), `VarRef("${exp.var}")`))) :
-						makeFailure("atomic tree failure")
+const makeAtomicDecl = (exp: AtomicExp): NodeDecl =>
+	isNumExp(exp) ? makeNodeDecl(varMakeByTag(exp.tag), `number(${exp.val})`) :
+		isBoolExp(exp) ? makeNodeDecl(varMakeByTag(exp.tag), `boolean("${exp.val}")`) :
+			isStrExp(exp) ? makeNodeDecl(varMakeByTag(exp.tag), `string("${exp.val}")`) :
+				isPrimOp(exp) ? makeNodeDecl(varMakeByTag(exp.tag), `PrimOp("${exp.op}")`) :
+					makeNodeDecl(varMakeByTag(exp.tag), `VarRef("${exp.var}")`)
 
 
 const createProgram = (prog: Program, expsId: string): Edge[] =>
-	makeEdge(makeNodeDecl(counter("program"), "Program"), makeNodeDecl(expsId, ":"), "exps").concat(concatAll(map(createExp, prog.exps)))
+	[makeEdge(
+		makeNodeDecl(varMakeByTag(prog.tag), "Program"),
+		makeNodeDecl(expsId, ":"), "exps")].concat(
+			listLoop(prog.exps, expsId))
 
-const concatAll = (array: Edge[][]): Edge[] =>
-	array.reduce(concat, [])
+const listLoop = (exps: Exp[], father: string): Edge[] =>
+	isEmpty(exps) ? [] : createExpEdges(exps[0], father).concat(listLoop(rest(exps), father))
 
-const createExp = (exp: Exp | SExpValue, father: string): Edge[] => {
-	isDefineExp(exp) ? createDefine(exp, father) :
-		isProcExp(exp) ? createProc(exp, counter("ProcExp"), counter("Params")) :
-			isLitExp(exp) ? createLit(exp) :
-				undefined
-}
+const listLoopVarDecl = (vars: VarDecl[], father: string): Edge[] =>
+	isEmpty(vars) ? [] : createExpEdges(vars[0], father).concat(listLoopVarDecl(rest(vars), father))
 
-const createDefine = (exp: DefineExp, father: string): Edge[] =>
-	father === "" ? [makeEdge(
-		makeNodeDecl(counter("DefineExp"), "DefineExp"), 
-		makeNodeDecl(counter("Var"), `[VarDecl(${exp.var.var})]`), "var")].concat(
+const createVarDeclEdge = (vardecl: VarDecl, id: string): Edge[] =>
+	[makeEdge(
+		makeNodeRef(id),
+		makeNodeDecl(counter("Var"), `["VarDecl(${vardecl.var})"]`))]
+
+const createExpEdges = (exp: Exp | SExpValue | VarDecl, father: string): Edge[] =>
+	isAtomicExp(exp) ? createAtomicEdge(exp, father) :
+		isDefineExp(exp) ? createDefine(exp, father, varMakeByTag("Define"), varMakeByTag("Var"), varMakeByTag(exp.val.tag)) :
+			isProcExp(exp) ? createProc(exp, father, varMakeByTag(exp.tag), varMakeByTag("Params"), varMakeByTag("Body")) :
+				//isLitExp(exp) 
+				//isAppExp(exp) 
+				//isIfExp(exp) 
+				//isLetExp(exp) 
+				//isLetrecExp(exp) 
+				//isSetExp(exp) 
+				isVarDecl(exp) ? createVarDeclEdge(exp, father) :
+					[]
+
+const createAtomicEdge = (exp: AtomicExp, father: string): Edge[] =>
+	isNumExp(exp) ? [makeEdge(makeNodeRef(father), makeNodeDecl(varMakeByTag(exp.tag), `["number(${exp.val})"]`))] :
+		isBoolExp(exp) ? [makeEdge(makeNodeRef(father), makeNodeDecl(varMakeByTag(exp.tag), `["boolean(${exp.val})"]`))] :
+			isStrExp(exp) ? [makeEdge(makeNodeRef(father), makeNodeDecl(varMakeByTag(exp.tag), `["string(${exp.val})"]`))] :
+				isPrimOp(exp) ? [makeEdge(makeNodeRef(father), makeNodeDecl(varMakeByTag(exp.tag), `["PrimOp(${exp.op})"]`))] :
+					[makeEdge(makeNodeRef(father), makeNodeDecl(varMakeByTag(exp.tag), `["VarRef(${exp.var})"]`))]
+
+
+const createDefine = (defineExp: DefineExp, father: string, defineID: string, varID: string, valID: string): Edge[] =>		//what if the define is atomic
+	isAtomicExp(defineExp.val) ?//var or val cosema shlahem??
+		father === "" ? [makeEdge(											//asumes it is not atomic
+			makeNodeDecl(defineID, "DefineExp"),
+			makeNodeDecl(varID, `["VarDecl(${defineExp.var.var})"]`), "var")].concat(
+				[makeEdge(
+					makeNodeRef(defineID),
+					makeNodeDecl(valID, defineExp.val.tag), "val")]).concat(
+						createExpEdges(defineExp.val, valID))
+			:
 			[makeEdge(
-				makeNodeRef(getCounter("DefineExp")), 
-				makeNodeDecl(counter(exp.val.tag), exp.val.tag), "val")]).concat(
-					createExp(exp.val, getCounter(exp.val.tag))) :
+				makeNodeRef(father),
+				makeNodeDecl(defineID, "DefineExp"))].concat(
+					[makeEdge(
+						makeNodeRef(defineID),
+						makeNodeDecl(varID, `["VarDecl(${defineExp.var.var})"]`), "var")]).concat(
+							[makeEdge(
+								makeNodeRef(defineID),
+								makeNodeDecl(valID, defineExp.val.tag), "val")]).concat(
+									createExpEdges(defineExp.val, valID))
+		:	//the value is atomic 
+		father === "" ? [makeEdge(											//asumes it is not atomic
+			makeNodeDecl(defineID, "DefineExp"),
+			makeNodeDecl(varID, `["VarDecl(${defineExp.var.var})"]`), "var")].concat(
+				[makeEdge(
+					makeNodeRef(defineID),
+					makeNodeDecl(valID, defineExp.val.tag), "val")]).concat(
+						createExpEdges(defineExp.val, valID))
+			:
+			[makeEdge(
+				makeNodeRef(father),
+				makeNodeDecl(defineID, "DefineExp"))].concat(
+					[makeEdge(
+						makeNodeRef(defineID),
+						makeNodeDecl(varID, `["VarDecl(${defineExp.var.var})"]`), "var")]).concat(
+							[makeEdge(
+								makeNodeRef(defineID),
+								makeNodeDecl(valID, defineExp.val.tag), "val")]).concat(
+									createExpEdges(defineExp.val, valID))
+
+
+const createProc = (exp: ProcExp, father: string, procID: string, paramsID: string, bodyID: string): Edge[] =>
+	father === "" ? [makeEdge(
+		makeNodeDecl(procID, "ProcExp"),
+		makeNodeDecl(paramsID, ":"), "args")].concat(
+			[makeEdge(
+				makeNodeRef(procID),
+				makeNodeDecl(bodyID, ":"), "body")]).concat(
+					listLoopVarDecl(exp.args, paramsID)).concat(
+						listLoop(exp.body, bodyID))
+		:
 		[makeEdge(
 			makeNodeRef(father),
-			makeNodeDecl(counter(exp.tag), "DefineExp"))].concat(
+			makeNodeDecl(procID, "ProcExp"))].concat(
 				[makeEdge(
-					makeNodeRef(getCounter(exp.tag)), 
-					makeNodeDecl(counter("Var"), `[VarDecl(${exp.var.var})]`), "var")]).concat(
+					makeNodeRef(procID),
+					makeNodeDecl(paramsID, ":"), "args")]).concat(
 						[makeEdge(
-							makeNodeRef(getCounter(exp.tag)), 
-							createNodeDecl(exp.val), "val")]).concat(
-								createExp(exp.val, getCounter(exp.val.tag)))
+							makeNodeRef(procID),
+							makeNodeDecl(bodyID, ":"), "body")]).concat(
+								listLoopVarDecl(exp.args, paramsID)).concat(
+									listLoop(exp.body, bodyID))
 
-const createProc = (exp: ProcExp, id: string, paramsID: string): Edge[] =>
-	[makeEdge(makeNodeDecl(id, "ProcExp"), makeNodeDecl(paramsID, ":"), "args")].concat([makeEdge(makeNodeRef(id), createNodeDecl(exp.body[0]), ":")]).concat(createVarDeclList(exp.args, paramsID)).concat(concatAll(map(createExp, exp.body)))
 
-const createLit = (exp: LitExp): Edge[] =>
-	[makeEdge(createNodeDecl(exp), createNodeDecl(exp.val), "val")].concat(createExp(exp.val.toString))
+//const createLit = (exp: LitExp): Edge[] =>
 
-const createVarDeclList = (vars: VarDecl[], id: string): Edge[] =>
-	isEmpty(vars) ? [] :
-		[makeEdge(makeNodeRef(id), makeNodeDecl(counter("Var"), `[VarDecl(${vars[0].var})]`))].concat(createVarDeclList(rest(vars), id))
 
-const createNodeDecl = (exp: CExp | SExpValue): NodeDecl => {
-	isLitExp(exp) ? makeNodeDecl(counter("LitExp"), "LitExp") :
-		isCompoundSExp(exp) ? makeNodeDecl(counter("CompoundSExp"), "CompoundSExp") :
-			undefined
-}
 
-let count: number = 0;
-let Program: number = 0;
-let Exps: number = 0;
-let PrimOp: number = 0;
-let VarRef: number = 0;
-let VarDecl: number = 0;
-let AppExp: number = 0;
-let LitExp: number = 0;
+// let count: number = 0;
+// let Program: number = 0;
+// let Exps: number = 0;
+// let PrimOp: number = 0;
+// let VarRef: number = 0;
+// let VarDecl: number = 0;
+// let AppExp: number = 0;
+// let LitExp: number = 0;
 
-export const counter = (s: string): string => {
-	switch (s) {
-		case "Program": Program++; count = Program
-		case "Exps": Exps++; count = Exps
-		case "PrimOp": PrimOp++; count = PrimOp
-		case "VarRef": VarRef++; count = VarRef
-		case "VarDecl": VarDecl++; count = VarDecl
-		case "AppExp": AppExp++; count = AppExp
-		case "LitExp": LitExp++; count = LitExp
-			break;
-		default:
-			break;
-	}
+// export const counter = (s: string): string => {
+// 	switch (s) {
+// 		case "Program": Program++; count = Program
+// 		case "Exps": Exps++; count = Exps
+// 		case "PrimOp": PrimOp++; count = PrimOp
+// 		case "VarRef": VarRef++; count = VarRef
+// 		case "VarDecl": VarDecl++; count = VarDecl
+// 		case "AppExp": AppExp++; count = AppExp
+// 		case "LitExp": LitExp++; count = LitExp
+// 			break;
+// 		default:
+// 			break;
+// 	}
 
-	return `${s}__${count}`
-}
+// 	return `${s}__${count}`
+// }
 
-export const getCounter = (s: string): string => {
-	switch (s) {
-		case "Program": count = Program
-		case "Exps": count = Exps
-		case "PrimOp": count = PrimOp
-		case "VarRef": count = VarRef
-		case "VarDecl": count = VarDecl
-		case "AppExp": count = AppExp
-		case "LitExp": count = LitExp
-			break;
-		default:
-			break;
-	}
+// export const getCounter = (s: string): string => {
+// 	switch (s) {
+// 		case "Program": count = Program
+// 		case "Exps": count = Exps
+// 		case "PrimOp": count = PrimOp
+// 		case "VarRef": count = VarRef
+// 		case "VarDecl": count = VarDecl
+// 		case "AppExp": count = AppExp
+// 		case "LitExp": count = LitExp
+// 			break;
+// 		default:
+// 			break;
+// 	}
 
-	return `${s}__${count}`
-}
+// 	return `${s}__${count}`
+// }
+
+//app, let
+
+
+const counterProgram = makeVarGen();
+const counterDefine = makeVarGen();
+const counterNumExp = makeVarGen();
+const counterBoolExp = makeVarGen();
+const counterStrExp = makeVarGen();
+const counterPrimOp = makeVarGen();
+const counterVarRef = makeVarGen();
+const counterVarDecl = makeVarGen();
+const counterAppExp = makeVarGen();
+const counterIfExp = makeVarGen();
+const counterProcExp = makeVarGen();
+const counterBinding = makeVarGen();
+const counterLetExp = makeVarGen();
+const counterLitExp = makeVarGen();
+const counterLetrecExp = makeVarGen();
+const counterSetExp = makeVarGen();
+const counterVarID = makeVarGen();
+
+export const varMakeByTag = (exp: string): string =>
+	exp === "ProgramExp" ? counterProgram("ProgramExp") :
+		exp === "DefineExp" ? counterDefine("DefineExp") :
+			"something"
