c8dc2dcd1e98ebfb46157d604f399a0b3ca5fe76
Haim <48633669+HaimTempo@users.noreply.github.com>
2020-06-10 20:04:05 +0300

    yay

--- hw4/part2/L5-eval.ts
+++ hw4/part2/L5-eval.ts
@@ -1,14 +1,18 @@
 // L5-eval-box
 
-import { map, repeat, zipWith } from "ramda";
-import { CExp, Exp, IfExp, LetrecExp, LetExp, ProcExp, Program, SetExp, isCExp, isLetValueExp, LetValueExp, ValuesBinding } from './L5-ast';
+import { map, repeat, zipWith, chain, match } from "ramda";
+import { CExp, Exp, IfExp, LetrecExp, LetExp, ProcExp, Program, SetExp, isCExp, isLetValueExp, LetValueExp, ValuesBinding, isVarDecl } from './L5-ast';
 import { Binding, VarDecl } from "./L5-ast";
 import { isBoolExp, isLitExp, isNumExp, isPrimOp, isStrExp, isVarRef } from "./L5-ast";
 import { parseL5Exp } from "./L5-ast";
-import { isAppExp, isDefineExp, isIfExp, isLetrecExp, isLetExp,
-         isProcExp, isSetExp } from "./L5-ast";
-import { applyEnv, applyEnvBdg, globalEnvAddBinding, makeExtEnv, setFBinding,
-         theGlobalEnv, Env, FBinding } from "./L5-env";
+import {
+    isAppExp, isDefineExp, isIfExp, isLetrecExp, isLetExp,
+    isProcExp, isSetExp
+} from "./L5-ast";
+import {
+    applyEnv, applyEnvBdg, globalEnvAddBinding, makeExtEnv, setFBinding,
+    theGlobalEnv, Env, FBinding
+} from "./L5-env";
 import { isClosure, makeClosure, Closure, Value, isTuple, Tuple, SExpValue } from "./L5-value";
 import { isEmpty, first, rest, allT } from '../shared/list';
 import { Result, makeOk, makeFailure, mapResult, safe2, bind, isFailure, safe3 } from "../shared/result";
@@ -20,59 +24,64 @@ import { applyPrimitive } from "./evalPrimitive";
 
 export const applicativeEval = (exp: CExp, env: Env): Result<Value> =>
     isNumExp(exp) ? makeOk(exp.val) :
-    isBoolExp(exp) ? makeOk(exp.val) :
-    isStrExp(exp) ? makeOk(exp.val) :
-    isPrimOp(exp) ? makeOk(exp) :
-    isVarRef(exp) ? applyEnv(env, exp.var) :
-    isLitExp(exp) ? makeOk(exp.val) :
-    isIfExp(exp) ? evalIf(exp, env) :
-    isProcExp(exp) ? evalProc(exp, env) :
-    isLetExp(exp) ? evalLet(exp, env) :
-    isLetValueExp(exp) ? evalLetValues(exp , env) :     //added
-    isLetrecExp(exp) ? evalLetrec(exp, env) :
-    isSetExp(exp) ? evalSet(exp, env) :
-    isAppExp(exp) ? safe2((proc: Value, args: Value[]) => applyProcedure(proc, args))
-                        (applicativeEval(exp.rator, env), mapResult(rand => applicativeEval(rand, env), exp.rands)) :
-    makeFailure(`Bad L5 AST ${exp}`);
+        isBoolExp(exp) ? makeOk(exp.val) :
+            isStrExp(exp) ? makeOk(exp.val) :
+                isPrimOp(exp) ? makeOk(exp) :
+                    isVarRef(exp) ? applyEnv(env, exp.var) :
+                        isLitExp(exp) ? makeOk(exp.val) :
+                            isIfExp(exp) ? evalIf(exp, env) :
+                                isProcExp(exp) ? evalProc(exp, env) :
+                                    isLetExp(exp) ? evalLet(exp, env) :
+                                        isLetValueExp(exp) ? evalLetValues(exp, env) :     //added
+                                            isLetrecExp(exp) ? evalLetrec(exp, env) :
+                                                isSetExp(exp) ? evalSet(exp, env) :
+                                                    isAppExp(exp) ? safe2((proc: Value, args: Value[]) => applyProcedure(proc, args))
+                                                        (applicativeEval(exp.rator, env), mapResult(rand => applicativeEval(rand, env), exp.rands)) :
+                                                        makeFailure(`Bad L5 AST ${exp}`);
 
-    // export interface LetValueExp { tag: "LetValueExp"; bindings: ValuesBinding[]; body: CExp[]; }
+// export interface LetValueExp { tag: "LetValueExp"; bindings: ValuesBinding[]; body: CExp[]; }
 // export interface ValuesBinding { tag: "ValuesBinding"; vars: VarDecl[]; val: CExp; }
 // interface tupel :  val: SExpValue[];
 
 
-const evalLetValues = (exp : LetValueExp, env:Env) : Result<Value> => {
-    const evaluatedValues = mapResult( (binding: ValuesBinding)=>
-        applicativeEval(binding.val, env) , exp.bindings)  //each element should be a tuple
-    const tuples = bind(evaluatedValues, makeTuples)
+const evalLetValues = (exp: LetValueExp, env: Env): Result<Value> =>
+    safe2((vars: VarDecl[][], vals: Tuple[]): Result<Value> =>
+        bind(balancedArrays2(vars, vals), bool =>
+            evalSequence(exp.body, makeExtEnv(combineVar(vars), combineVal(vals), env))))
+        (mapResult((b: ValuesBinding) => makeOk(b.vars), exp.bindings)
+            , tuples(exp.bindings, env))
+
+const tuples = (bnd: ValuesBinding[], env: Env): Result<Tuple[]> =>
+    bind(mapResult((binding: ValuesBinding) =>
+        applicativeEval(binding.val, env), bnd), makeTuples)
+
+const balancedArrays2 = (vars: VarDecl[][], vals: Tuple[]): Result<boolean> =>
+    vars.length != vals.length  ? makeFailure('let values had defrent sizes!!!') :
+        balancedArrays(vars, vals)
 
 
+const balancedArrays = (vars: VarDecl[][], vals: Tuple[]): Result<boolean> =>
+    vars.length === 0 && vals.length === 0 ? makeOk(true) :
+        vars[0].length !== vals[0].val.length ? makeFailure('let values size didnt match ${vars[0]} != ${vals[0]}') :
+            balancedArrays(rest(vars), rest(vals))
 
-
-    safe2( )
-    // (combineVar(exp.bindings), combineVal(tuples), env);
-    evalSequence(exp.body, newEnv)
-    return makeFailure('shit')
-}
-// safe2((vars : VarDecl[][] ,val : Tuple[]) => 
-// evalSequence(exp.body, makeExtEnv(combine(vars), combineVal(val), env))
-
-const combineVar = (vars:ValuesBinding[]): string[] =>
-    ['shit']
+const combineVar = (vars: VarDecl[][]): string[] =>
+    chain((x) => map((v: VarDecl) => v.var, x), vars);
 
 // make one long array
-const combineVal = (values:Result<Tuple[]>): Result<SExpValue[]> =>
-    makeFailure('shit')
+const combineVal = (vals: Tuple[]): SExpValue[] => chain((x) => x.val, vals);
 
-
-const makeTuples = (values:SExpValue[]): Result<Tuple[]> =>
+const makeTuples = (values: SExpValue[]): Result<Tuple[]> =>
     allT(isTuple, values) ? makeOk(values) : makeFailure('expected values in let values ${JSON.stringify(v)}')
 
+
+
 export const isTrueValue = (x: Value): boolean =>
-    ! (x === false);
+    !(x === false);
 
 const evalIf = (exp: IfExp, env: Env): Result<Value> =>
     bind(applicativeEval(exp.test, env),
-         (test: Value) => isTrueValue(test) ? applicativeEval(exp.then, env) : applicativeEval(exp.alt, env));
+        (test: Value) => isTrueValue(test) ? applicativeEval(exp.then, env) : applicativeEval(exp.alt, env));
 
 const evalProc = (exp: ProcExp, env: Env): Result<Closure> =>
     makeOk(makeClosure(exp.args, exp.body, env));
@@ -81,8 +90,8 @@ const evalProc = (exp: ProcExp, env: Env): Result<Closure> =>
 //      Instead we use the env of the closure.
 const applyProcedure = (proc: Value, args: Value[]): Result<Value> =>
     isPrimOp(proc) ? applyPrimitive(proc, args) :
-    isClosure(proc) ? applyClosure(proc, args) :
-    makeFailure(`Bad procedure ${JSON.stringify(proc)}`);
+        isClosure(proc) ? applyClosure(proc, args) :
+            makeFailure(`Bad procedure ${JSON.stringify(proc)}`);
 
 const applyClosure = (proc: Closure, args: Value[]): Result<Value> => {
     const vars = map((v: VarDecl) => v.var, proc.params);
@@ -92,14 +101,14 @@ const applyClosure = (proc: Closure, args: Value[]): Result<Value> => {
 // Evaluate a sequence of expressions (in a program)
 export const evalSequence = (seq: Exp[], env: Env): Result<Value> =>
     isEmpty(seq) ? makeFailure("Empty sequence") :
-    isDefineExp(first(seq)) ? evalDefineExps(first(seq), rest(seq)) :
-    evalCExps(first(seq), rest(seq), env);
-    
+        isDefineExp(first(seq)) ? evalDefineExps(first(seq), rest(seq)) :
+            evalCExps(first(seq), rest(seq), env);
+
 const evalCExps = (first: Exp, rest: Exp[], env: Env): Result<Value> =>
     isCExp(first) && isEmpty(rest) ? applicativeEval(first, env) :
-    isCExp(first) ? bind(applicativeEval(first, env), _ => evalSequence(rest, env)) :
-    makeFailure("Never");
-    
+        isCExp(first) ? bind(applicativeEval(first, env), _ => evalSequence(rest, env)) :
+            makeFailure("Never");
+
 // define always updates theGlobalEnv
 // We also only expect defineExps at the top level.
 // Eval a sequence of expressions when the first exp is a Define.
@@ -107,9 +116,11 @@ const evalCExps = (first: Exp, rest: Exp[], env: Env): Result<Value> =>
 // then compute the rest of the exps in the new env.
 const evalDefineExps = (def: Exp, exps: Exp[]): Result<Value> =>
     isDefineExp(def) ? bind(applicativeEval(def.val, theGlobalEnv),
-                            (rhs: Value) => { globalEnvAddBinding(def.var.var, rhs);
-                                              return evalSequence(exps, theGlobalEnv); }) :
-    makeFailure("Unexpected " + def);
+        (rhs: Value) => {
+            globalEnvAddBinding(def.var.var, rhs);
+            return evalSequence(exps, theGlobalEnv);
+        }) :
+        makeFailure("Unexpected " + def);
 
 // Main program
 export const evalProgram = (program: Program): Result<Value> =>
@@ -121,7 +132,7 @@ export const evalParse = (s: string): Result<Value> =>
 // LET: Direct evaluation rule without syntax expansion
 // compute the values, extend the env, eval the body.
 const evalLet = (exp: LetExp, env: Env): Result<Value> => {
-    const vals = mapResult((v : CExp) => applicativeEval(v, env), map((b : Binding) => b.val, exp.bindings));
+    const vals = mapResult((v: CExp) => applicativeEval(v, env), map((b: Binding) => b.val, exp.bindings));
     const vars = map((b: Binding) => b.var.var, exp.bindings);
     return bind(vals, (vals: Value[]) => evalSequence(exp.body, makeExtEnv(vars, vals, env)));
 }
@@ -138,7 +149,7 @@ const evalLetrec = (exp: LetrecExp, env: Env): Result<Value> => {
     // @@ Compute the vals in the extended env
     const cvalsResult = mapResult((v: CExp) => applicativeEval(v, extEnv), vals);
     const result = bind(cvalsResult,
-                        (cvals: Value[]) => makeOk(zipWith((bdg, cval) => setFBinding(bdg, cval), extEnv.frame.fbindings, cvals)));
+        (cvals: Value[]) => makeOk(zipWith((bdg, cval) => setFBinding(bdg, cval), extEnv.frame.fbindings, cvals)));
     return bind(result, _ => evalSequence(exp.body, extEnv));
 };
 
